9dd3fde04b1334b4525b272db8a779410347df83
NeoMabasa1@gmail.com <neomabasa1@gmail.com>
2023-12-11 19:25:25 +0200

    correctly name js files

--- CalculateFibonacci.js
+++ CalculateFibonacci.js
@@ -1,22 +1,21 @@
 function generateFibonacci(n) {
     // Initialize an array with the first two Fibonacci numbers
     const fibonacci = [0, 1];
-  
+
     // Use a for loop to calculate the next Fibonacci numbers
     for (let i = 2; i < n; i++) {
       const nextFibonacci = fibonacci[i - 1] + fibonacci[i - 2];
       fibonacci.push(nextFibonacci);
     }
-  
+
     return fibonacci;
   }
-  
+
   // Call the function with an appropriate value of n
   const n = 10; // Change this to the desired number of Fibonacci numbers
   const fibonacciSequence = generateFibonacci(n);
-  
+
   // Log the result
   console.log(`The first ${n} Fibonacci numbers are:`, fibonacciSequence);
-  
-  
-  
+
+
